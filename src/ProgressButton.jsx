55d81c5cd6357d032726e5577b8d189ca3ecda31
Johan Munneke <johanmunneke94@gmail.com>
2024-11-22 15:06:20 +0100

    Fixed upgrade and included the lib in the project

--- src/ProgressButton.jsx
+++ src/ProgressButton.jsx
@@ -8,9 +8,9 @@ export default class ProgressButton extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            buttonState: '',
+            buttonState: "",
             buttonWidth: null,
-            buttonHeight: null         
+            buttonHeight: null
         };
         this.onButtonClick = this.clickButton.bind(this);
         this.onError = this.errorAction.bind(this);
@@ -18,40 +18,43 @@ export default class ProgressButton extends Component {
         this.progressButton = createRef();
     }
 
-    componentDidUpdate (prevProps) {
-        // If component is currently in state loading, the button has been clicked before. 
+    componentDidUpdate(prevProps) {
+        // If component is currently in state loading, the button has been clicked before.
         // Check whether the prop actionSucceeded did change and change the state
-        if (this.state.buttonState === 'loading' && this.props.actionSucceeded.value !== prevProps.actionSucceeded.value){
-            if (this.props.actionSucceeded.value === 'success') {
-                this.setState({buttonState: 'success'});
-            } else if (this.props.actionSucceeded.value === 'error') {
-                this.setState({buttonState: 'error'});
+        if (
+            this.state.buttonState === "loading" &&
+            this.props.actionSucceeded.value !== prevProps.actionSucceeded.value
+        ) {
+            if (this.props.actionSucceeded.value === "success") {
+                this.setState({ buttonState: "success" });
+            } else if (this.props.actionSucceeded.value === "error") {
+                this.setState({ buttonState: "error" });
             } else {
-                this.setState({buttonState: ''});
+                this.setState({ buttonState: "" });
             }
-            this.props.actionSucceeded.setValue('');
-        // Check if the disabled prop changed and if so, disable or enable the button
+            this.props.actionSucceeded.setValue("");
+            // Check if the disabled prop changed and if so, disable or enable the button
         } else if (this.props.buttonDisabled !== prevProps.buttonDisabled) {
             if (this.props.buttonDisabled.value) {
-                this.setState({buttonState: 'disabled'});
+                this.setState({ buttonState: "disabled" });
             } else {
-                this.setState({buttonState: ''});
+                this.setState({ buttonState: "" });
             }
         }
     }
 
     clickButton() {
         //check if button not already clicked
-        if (this.state.buttonState === '') {
+        if (this.state.buttonState === "") {
             //Get height and width of button, and set them to the state, such that render can use these dimensions
             var height = this.progressButton.current.clientHeight;
             var width = this.progressButton.current.clientWidth;
-            this.setState({buttonWidth: width});
-            this.setState({buttonHeight: height});
+            this.setState({ buttonWidth: width });
+            this.setState({ buttonHeight: height });
             // Execute the onclick action if there is one
             if (this.props.onClickAction && this.props.onClickAction.canExecute) {
                 //Switch layout to loading, untill this.props.actionSucceeded is changed
-                this.setState({buttonState: 'loading'});
+                this.setState({ buttonState: "loading" });
                 this.props.onClickAction.execute();
             }
         }
@@ -63,7 +66,7 @@ export default class ProgressButton extends Component {
             this.props.onError.execute();
         }
         //Set state back to initial value
-        this.setState({buttonState: ''});
+        this.setState({ buttonState: "" });
     }
 
     successAction() {
@@ -72,26 +75,26 @@ export default class ProgressButton extends Component {
             this.props.onSuccess.execute();
         }
         //Set state back to initial value
-        this.setState({buttonState: ''});
+        this.setState({ buttonState: "" });
     }
 
     render() {
         //determine classes (also get mendix classes)
-        var className = this.props.buttonType + 'pb ' + this.props.class;
-        //Intialize style object to set the width and height. 
+        var className = this.props.buttonType + "pb " + this.props.class;
+        //Intialize style object to set the width and height.
         //Height will only be set after the first buttonclick, to make sure it won't change in another state
         var buttonStyle = {};
         //determine width if not yet initialized
         if (this.state.buttonWidth === null) {
-            var buttonWidth = '';
-            if (this.props.buttonWidth === 'fitcontent') {
-                buttonWidth = 'fit-content';
-            } else if (this.props.buttonWidth === 'fullwidth') {
-                buttonWidth = '100%';
-            } else if (this.props.buttonWidth === 'pixels') {
-                buttonWidth = this.props.percentageOrPixels + 'px';
+            var buttonWidth = "";
+            if (this.props.buttonWidth === "fitcontent") {
+                buttonWidth = "fit-content";
+            } else if (this.props.buttonWidth === "fullwidth") {
+                buttonWidth = "100%";
+            } else if (this.props.buttonWidth === "pixels") {
+                buttonWidth = this.props.percentageOrPixels + "px";
             } else {
-                buttonWidth = this.props.percentageOrPixels + '%';
+                buttonWidth = this.props.percentageOrPixels + "%";
             }
             buttonStyle.width = buttonWidth;
         } else {
@@ -99,25 +102,25 @@ export default class ProgressButton extends Component {
         }
         //determine height if not yet initialized
         if (this.state.buttonHeight !== null) {
-            buttonStyle.height =  this.state.buttonHeight;
+            buttonStyle.height = this.state.buttonHeight;
         }
         //set display value to table, since fit-content is not cross-browser compatible
-        buttonStyle.display = 'table';
+        buttonStyle.display = "table";
 
-        return <div 
-            className = {className}
-            style = {buttonStyle}
-            ref = {this.progressButton}
-            >
-            <ProgressButtonUI 
-                buttonText = {this.props.buttonText.value}
-                buttonState = {this.state.buttonState}
-                onButtonClick = {this.onButtonClick}
-                durationError = {this.props.durationError}
-                durationSuccess = {this.props.durationSuccess}
-                onError = {this.onError}
-                onSuccess = {this.onSuccess} 
-            />
-        </div>;
+        return (
+            <div className={className} style={buttonStyle} ref={this.progressButton}>
+                <ProgressButtonUI
+                    state={this.state.buttonState}
+                    onClick={this.onButtonClick}
+                    durationError={this.props.durationError}
+                    durationSuccess={this.props.durationSuccess}
+                    onError={this.onError}
+                    onSuccess={this.onSuccess}
+                    controlled="true"
+                >
+                    {this.props.buttonText.value}
+                </ProgressButtonUI>
+            </div>
+        );
     }
 }
--- src/components/ProgressButtonUI.jsx
+++ src/components/ProgressButtonUI.jsx
@@ -1,22 +1,139 @@
-import { Component, createElement } from "react";
-import ProgressButton from 'react-progress-button'
+import { createElement, useState, useEffect } from "react";
 
-export class ProgressButtonUI extends Component {
-    constructor(props) {
-        super(props);
-    }
+export const possibleStates = {
+    LOADING: "loading",
+    DISABLED: "disabled",
+    SUCCESS: "success",
+    ERROR: "error",
+    NOTHING: ""
+};
 
-    render() {
-        return <ProgressButton 
-            onClick = {this.props.onButtonClick}
-            state = {this.props.buttonState}
-            durationError = {this.props.durationError}
-            durationSuccess = {this.props.durationSuccess}
-            onError = {this.props.onError}
-            onSuccess = {this.props.onSuccess}
-            controlled = 'true'
+export const ProgressButtonUI = ({
+    classNamespace = "pb-",
+    controlled = false,
+    durationError = 1200,
+    durationSuccess = 500,
+    form,
+    onClick = () => {},
+    onError = () => {},
+    onSuccess = () => {},
+    state = possibleStates.NOTHING,
+    type,
+    shouldAllowClickOnLoading = false,
+    className,
+    children,
+    ...containerProps
+}) => {
+    const [currentState, setCurrentState] = useState(state);
+
+    const loading = () => {
+        setCurrentState(possibleStates.LOADING);
+    };
+
+    const notLoading = () => {
+        setCurrentState(possibleStates.NOTHING);
+    };
+
+    const disable = () => {
+        setCurrentState(possibleStates.DISABLED);
+    };
+
+    const success = (callback = onSuccess, dontRemove) => {
+        setCurrentState(possibleStates.SUCCESS);
+        setTimeout(() => {
+            if (!dontRemove) setCurrentState(possibleStates.NOTHING);
+            if (typeof callback === "function") callback();
+        }, durationSuccess);
+    };
+
+    const error = (callback = onError, err) => {
+        setCurrentState(possibleStates.ERROR);
+        setTimeout(() => {
+            setCurrentState(possibleStates.NOTHING);
+            if (typeof callback === "function") callback(err);
+        }, durationError);
+    };
+
+    useEffect(() => {
+        if (state === currentState) return;
+
+        switch (state) {
+            case possibleStates.SUCCESS:
+                success();
+                break;
+            case possibleStates.ERROR:
+                error();
+                break;
+            case possibleStates.LOADING:
+                loading();
+                break;
+            case possibleStates.DISABLED:
+                disable();
+                break;
+            case possibleStates.NOTHING:
+                notLoading();
+                break;
+            default:
+                break;
+        }
+    }, [state]);
+
+    const handlePromise = promise => {
+        if (promise && promise.then && promise.catch) {
+            promise
+                .then(() => {
+                    success();
+                })
+                .catch(err => {
+                    error(null, err);
+                });
+        }
+    };
+    const handleClick = e => {
+        if (controlled) {
+            onClick(e);
+            return true;
+        }
+
+        if (
+            (shouldAllowClickOnLoading || currentState !== possibleStates.LOADING) &&
+            currentState !== possibleStates.DISABLED
+        ) {
+            loading();
+            const ret = onClick(e);
+            handlePromise(ret);
+        } else {
+            e.preventDefault();
+        }
+        return true;
+    };
+
+    containerProps.className = `${classNamespace}container ${currentState} ${className}`;
+    containerProps.onClick = handleClick;
+
+    return (
+        <div {...containerProps}>
+            <button
+                disabled={currentState === possibleStates.DISABLED}
+                type={type}
+                form={form}
+                className={`${classNamespace}button`}
             >
-                {this.props.buttonText}
-            </ProgressButton>;
-    }
-}
+                <span>{children}</span>
+                <svg className={`${classNamespace}progress-circle`} viewBox="0 0 41 41">
+                    <path d="M38,20.5 C38,30.1685093 30.1685093,38 20.5,38" />
+                </svg>
+                <svg className={`${classNamespace}checkmark`} viewBox="0 0 70 70">
+                    <path d="m31.5,46.5l15.3,-23.2" />
+                    <path d="m31.5,46.5l-8.5,-7.1" />
+                </svg>
+                <svg className={`${classNamespace}cross`} viewBox="0 0 70 70">
+                    <path d="m35,35l-9.3,-9.3" />
+                    <path d="m35,35l9.3,9.3" />
+                    <path d="m35,35l-9.3,9.3" />
+                    <path d="m35,35l9.3,-9.3" />
+                </svg>
+            </button>
+        </div>
+    );
+};
